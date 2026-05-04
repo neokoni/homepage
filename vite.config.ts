bd3bfe6df223492c82347cd39fd8c794d5892be6
neokoni <dulin081682@126.com>
2026-05-04 18:53:58 +0800

    depends: add tailwindcss
    Signed-off-by: neokoni <dulin081682@126.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -9,6 +10,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
