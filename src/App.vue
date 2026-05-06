7124f78f0ea40b5beb2451473294510d68bb2bdf
neokoni <dulin081682@126.com>
2026-05-06 15:54:54 +0800

    chore: add base info (?)
    Signed-off-by: neokoni <dulin081682@126.com>

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,8 @@ import LeftInfo from './router/LeftInfo.vue';
 import RightPart from './router/RightPart.vue';
 import SiteConfig from './configs/site.json';
 
+// set background
 document.body.style.backgroundImage = `url(${SiteConfig['background-img']})`;
-if (SiteConfig.title) {
-  document.title=SiteConfig.title;
-}
 </script>
 
 <template>
