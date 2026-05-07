65f7763c93d104bc1a2b0ee5e58808a3ec4cc078
neokoni <dulin081682@126.com>
2026-05-07 21:24:27 +0800

    chore: remove the right part of the homepage
    Signed-off-by: neokoni <dulin081682@126.com>

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import LeftInfo from './router/LeftInfo.vue';
-import RightPart from './router/RightPart.vue';
 import SiteConfig from './configs/site.json';
 
 // set background
@@ -8,8 +7,7 @@ document.body.style.backgroundImage = `url(${SiteConfig['background-img']})`;
 </script>
 
 <template>
-  <div class="flex flex-col h-screen w-screen">
+  <div class="flex h-screen w-screen">
     <LeftInfo />
-    <!-- <RightPart /> -->
   </div>
 </template>
