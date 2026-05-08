793ee5e7bf0af4f14a1f725f33e9168b11396be9
neokoni <dulin081682@126.com>
2026-05-08 16:51:04 +0800

    sytle: adapt to dark mode
    Signed-off-by: neokoni <dulin081682@126.com>

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,28 @@ import AuthorInfo from './configs/author.json';
 
 import { Icon } from '@iconify/vue'
 // set background
-document.body.style.backgroundImage = `url(${SiteConfig['background-img']})`;
+// document.body.style.backgroundImage = `url(${SiteConfig['background-img']})`;
 </script>
 
 <template>
   <div class="flex h-screen w-screen">
-    <div class="md:w-5/16 md:w-max-w-2/3 w-full h-full bg-white/50
-                backdrop-blur-2xl border-r border-white/50
+    <div class="md:w-5/16 md:w-max-w-2/3 w-full h-full 
+                bg-white/50 border-white/50
+                dark:bg-black/60 dark:border-gray-700/50
+                backdrop-blur-2xl border-r
                 shadow-[10px_0px_30px_rgba(0,0,0,0.25)]
                 flex flex-col items-center justify-center">
       <img :src="AuthorInfo.avatar" alt="avatar"
-        class="h-40 w-40 rounded-full mb-4 
-                border-r border-white/50 shadow-xl">
-      <h1 class="text-3xl font-bold mb-4">{{ AuthorInfo.name }}</h1>
-      <h1 class="text-lg text-gray-700 mb-6" v-if="AuthorInfo.bio">{{ AuthorInfo.bio }}</h1>
+        class="h-40 w-40 rounded-full mb-4 shadow-xl">
+      <h1 class="text-3xl font-bold mb-4
+                text-gray-900 dark:text-gray-200">{{ AuthorInfo.name }}</h1>
+      <h1 class="text-lg text-gray-700 dark:text-gray-300 mb-6" v-if="AuthorInfo.bio">{{ AuthorInfo.bio }}</h1>
       <div class="flex text-2xl gap-2">
         <template v-for="value in AuthorInfo.links">
           <a :href="value.url" target="_blank" 
-              class="group relative text-gray-700 hover:text-gray-900 ransition-all duration-300
+              class="text-gray-700 hover:text-gray-900 
+                    dark:text-gray-300 dark:hover:text-white
+                    ransition-all duration-300 group relative 
                     flex-col flex items-center justify-center hover:ml-2 hover:mr-2"
               :style="{
                 '--icon-default': value.defaultColor || undefined,
@@ -41,9 +45,11 @@ document.body.style.backgroundImage = `url(${SiteConfig['background-img']})`;
                 flex flex-col items-center justify-center">
       <template v-for="value in SiteConfig.icp" v-if="SiteConfig.icp">
         <a :href="value.url" target="_blank"
-          class="text-sm text-gray-700 hover:text-gray-900 transition-colors duration-300">{{ value.name }}</a>
+          class="text-gray-700 hover:text-gray-900
+              dark:text-gray-200 dark:hover:text-white
+              text-sm transition-colors duration-300">{{ value.name }}</a>
       </template>
-      <p class="text-sm text-gray-700" v-if="SiteConfig.right">&copy; {{ SiteConfig.right }}</p>
+      <p class="text-sm text-gray-700 dark:text-gray-100" v-if="SiteConfig.right">&copy; {{ SiteConfig.right }}</p>
     </footer>
   </div>
 </template>
