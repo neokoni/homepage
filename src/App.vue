248f7bac1e724ccec9656ec272ac8b1c024c7e82
neokoni <dulin081682@126.com>
2026-05-08 19:08:51 +0800

    feat: background info component
    Signed-off-by: neokoni <dulin081682@126.com>

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,41 @@ import SiteConfig from './configs/site.json';
 import AuthorInfo from './configs/author.json';
 
 import { Icon } from '@iconify/vue'
+import BackgroundInfo from './components/BackgroundInfo.vue';
+import { onBeforeUnmount, onMounted, ref } from 'vue';
 // set background
 var defaultBackground = `url(${SiteConfig['background-img']})`;
 var darkBackground = `url(${SiteConfig['darkmode-background-img']})`;
-console.log(darkBackground);
-if (darkBackground.length <= 5) { // url() is 5 characters
-  darkBackground = defaultBackground;
+const usingDarkBackground = ref(false);
+const setCurrentTheme = () => {
+  const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  usingDarkBackground.value = isDarkMode && darkBackground.length > 5; // url() is 5 characters
 }
+setCurrentTheme();
 document.documentElement.style.setProperty('--default-background', defaultBackground);
 document.documentElement.style.setProperty('--dark-background', darkBackground);
+
+let colorSchemeQuery: MediaQueryList | null = null;
+const handleColorSchemeChange = () => setCurrentTheme();
+
+onMounted(() => {
+  colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  if ('addEventListener' in colorSchemeQuery) {
+    colorSchemeQuery.addEventListener('change', handleColorSchemeChange);
+  } else {
+    colorSchemeQuery.addListener(handleColorSchemeChange);
+  }
+});
+
+onBeforeUnmount(() => {
+  if (!colorSchemeQuery) return;
+  if ('removeEventListener' in colorSchemeQuery) {
+    colorSchemeQuery.removeEventListener('change', handleColorSchemeChange);
+  } else {
+    colorSchemeQuery.removeListener(handleColorSchemeChange);
+  }
+});
+
 </script>
 
 <template>
@@ -19,7 +45,7 @@ document.documentElement.style.setProperty('--dark-background', darkBackground);
     <div class="md:w-5/16 md:w-max-w-2/3 w-full h-full 
                 bg-white/50 border-white/50
                 dark:bg-black/60 dark:border-gray-700/50
-                backdrop-blur-2xl border-r
+                backdrop-blur-2xl border-r transition-all duration-300
                 shadow-[10px_0px_30px_rgba(0,0,0,0.25)]
                 flex flex-col items-center justify-center">
       <img :src="AuthorInfo.avatar" alt="avatar"
@@ -58,6 +84,10 @@ document.documentElement.style.setProperty('--dark-background', darkBackground);
       </template>
       <p class="text-sm text-gray-700 dark:text-gray-100" v-if="SiteConfig.right">&copy; {{ SiteConfig.right }}</p>
     </footer>
+    <div v-if="SiteConfig.showBackgroundInfo" 
+        class="invisible md:visible right-0 bottom-0 absolute">
+        <BackgroundInfo :dark-mod="usingDarkBackground"/>
+      </div>
   </div>
 </template>
 
---
+++ src/components/BackgroundInfo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="bg-white/50 border-white/50
+                dark:bg-black/60 dark:border-gray-700/50
+                backdrop-blur-2xl border p-2
+                text-black/85 dark:text-gray-300
+                rounded-tl-lg text-sm">
+        <p>Background Info</p>
+        <p v-if="backgroundInfo[+prop.darkMod]?.author">Author: <a :href="backgroundInfo[+prop.darkMod]?.authorUrl" target="_blank">
+          {{ backgroundInfo[+prop.darkMod]?.author }}
+          </a>
+        </p>
+        
+        <p v-if="backgroundInfo[+prop.darkMod]?.name">Source: <a :href="backgroundInfo[+prop.darkMod]?.imgSource" target="_blank">
+          {{ backgroundInfo[+prop.darkMod]?.name }}
+        </a>
+        </p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import SiteConfig from '../configs/site.json';
+
+type BackgroundInfo = {
+  author: string;
+  authorUrl: string;
+  imgSource: string;
+  name: string;
+}
+const backgroundInfo = SiteConfig.backgroundInfo as BackgroundInfo[];
+const prop = defineProps<{
+  darkMod: boolean;
+}>();
+</script>
