cdb98dba553c8ba73b61062ee76cf37171b086cf
Neokoni <dulin081682@126.com>
2026-05-21 03:54:10 +0000

    feat: add hitokoto support

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,37 @@ import { onBeforeUnmount, onMounted, ref } from 'vue';
 // set background
 var defaultBackground = `url(${SiteConfig['background-img']})`;
 var darkBackground = `url(${SiteConfig['darkmode-background-img']})`;
+
 const usingDarkBackground = ref(false);
 const setCurrentTheme = () => {
   const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
   usingDarkBackground.value = isDarkMode && darkBackground.length > 5; // url() is 5 characters
 }
+
+const fetchHitokoto = async () => {
+  const hitokotoUrl = SiteConfig.hitokoto?.url || 'https://international.v1.hitokoto.cn';
+  const response = await fetch(hitokotoUrl);
+  const data = await response.json();
+  var hitokotoCtx = data.hitokoto || '';
+  var hitokotoFrom = data.from || '';
+  if (hitokotoCtx.length>0 && hitokotoFrom.length>0) {
+    document.getElementById('hitokotoCtx')!.textContent = `「${hitokotoCtx}」`;
+    document.getElementById('hitokotoFrom')!.textContent = `—— ${hitokotoFrom}`;
+    document.getElementById('hitokotoDiv')!.classList.remove('opacity-0');
+    document.getElementById('hitokotoDiv')!.classList.add('max-h-10');
+  }
+}
+
+const refreshHitokoto = () => {
+  document.getElementById('hitokotoDiv')!.classList.add('opacity-0');
+  document.getElementById('hitokotoDiv')!.classList.remove('max-h-10');
+  setTimeout(() => {
+    fetchHitokoto();
+  }, 500);
+}
+
 setCurrentTheme();
+fetchHitokoto();
 document.documentElement.style.setProperty('--default-background', defaultBackground);
 document.documentElement.style.setProperty('--dark-background', darkBackground);
 
@@ -29,7 +54,6 @@ onBeforeUnmount(() => {
   if (!colorSchemeQuery) return;
   colorSchemeQuery.removeEventListener('change', handleColorSchemeChange);
 });
-
 </script>
 
 <template>
@@ -44,7 +68,15 @@ onBeforeUnmount(() => {
         class="h-40 w-40 rounded-full mb-4 shadow-xl">
       <h1 class="text-3xl font-bold mb-4
                 text-gray-900 dark:text-gray-200">{{ AuthorInfo.name }}</h1>
-      <h1 class="text-lg text-gray-700 dark:text-gray-300 mb-6" v-if="AuthorInfo.bio">{{ AuthorInfo.bio }}</h1>
+      <h1 class="text-lg text-gray-700 dark:text-gray-300 mb-2" v-if="AuthorInfo.bio">{{ AuthorInfo.bio }}</h1>
+      <!-- 一言 -->
+      <div id="hitokotoDiv" v-if="SiteConfig.hitokoto.enable" 
+          class="duration-500 transition-all opacity-0 max-h-0 animation-pulse"
+          @click="refreshHitokoto()">
+        <h1 id="hitokotoCtx" class="text-sm text-gray-600 dark:text-gray-300">&nbsp</h1>
+        <h1 id="hitokotoFrom" class="text-sm text-gray-600 dark:text-gray-300 text-right">&nbsp</h1>
+      </div>
+      <!-- <h1 class="text-sm text-gray-700 dark:text-gray-300 mb-6" v-if="AuthorInfo.bio">yiyan</h1> -->
       <div class="flex text-2xl gap-2">
         <template v-for="value in AuthorInfo.links">
           <a :href="value.url" target="_blank" 
@@ -64,7 +96,7 @@ onBeforeUnmount(() => {
           </a>
         </template>
       </div>
-      <div class="md:h-0 h-10"></div>
+      <!-- </div> -->
     </div>
     <footer class="absolute bottom-0 md:w-5/16 w-full h-[-100px] mb-3
                 flex flex-col items-center justify-center">
